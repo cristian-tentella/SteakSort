06fbcae620b3bba0cd254a2a96bad81d811fe1e2
Cristian Tentella <cristiantentella.dev@pm.me>
2023-06-27 15:24:34 +0200

    chore: add build files to version control

---
+++ src/main/java/io/github/cristiantentella/steaksort/SteakSort.java
@@ -0,0 +1,17 @@
+package io.github.cristiantentella.steaksort;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SteakSort extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
